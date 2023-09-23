fb2ebf9abbe29a352fed46af22b72ae374aa426d
Mukul Gupta <79056592+mukul1357@users.noreply.github.com>
2023-09-24 02:32:45 +0530

    Update and rename ftest.py to Test.py

---
+++ Test.py
@@ -0,0 +1,14 @@
+import unittest
+
+from Prog1 import summation
+
+
+class TestSum(unittest.TestCase):
+    
+    def test_list_int(self):
+        data = [23, 32]
+        res = summation(data)
+        self.assertEqual(res, 55)
+
+if __name__ == "__main__":
+    unittest.main()
